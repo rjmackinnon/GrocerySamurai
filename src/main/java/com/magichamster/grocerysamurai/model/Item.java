ad8a20daecd76e2ce5ab4d642d9090db3e1f2643
rjmackinnon <rick.j.mackinnon@gmail.com>
2017-04-09 22:18:35 -0400

    First class! Woo hoo!

---
+++ src/main/java/com/magichamster/grocerysamurai/model/Item.java
@@ -0,0 +1,56 @@
+package com.magichamster.grocerysamurai.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by rick on 4/9/17.
+ */
+@Entity
+@Table(name = "item")
+public class Item {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "upc")
+    private long Upc;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getUpc() {
+        return Upc;
+    }
+
+    public void setUpc(long upc) {
+        Upc = upc;
+    }
+}
