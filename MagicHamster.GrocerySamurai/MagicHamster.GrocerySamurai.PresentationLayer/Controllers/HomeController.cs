adf5da8167146d304cb1288ee4154aea9aba1698
Rick MacKinnon <rjmackinnon@users.noreply.github.com>
2017-12-27 17:02:13 -0500

    Initial front end

---
+++ MagicHamster.GrocerySamurai/MagicHamster.GrocerySamurai.PresentationLayer/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MagicHamster.GrocerySamurai.PresentationLayer.Models;
+
+namespace MagicHamster.GrocerySamurai.PresentationLayer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Grocery Samurai";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Contact the good folks at MagicHamster.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
